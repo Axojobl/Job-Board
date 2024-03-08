a7bdb612e143bd0ea618b5086e8ef2c41ebecf88
adeebbayat <adeebbayat@yahoo.com>
2024-03-08 14:10:45 -0800

    final preGroup thang

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     rules: [
       {
         test: /\.jsx?$/,
-        exclude: /node_modules/,
+        exclude: /(node_modules)/,
         loader: "babel-loader",
         options: {
           presets: ["@babel/env", "@babel/react"],

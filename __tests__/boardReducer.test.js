de70100004301cba02daf9d7c381304abb0f4a13
Evan Griffith <evan@evangriffith.net>
2024-03-12 16:53:43 -0700

    Completed ADD_TO_STATE tests for boardReducer

--- __tests__/boardReducer.test.js
+++ __tests__/boardReducer.test.js
@@ -10,70 +10,76 @@
 
 import subject from '../client/reducers/boardReducer';
 
-
 describe('Job-Board boardReducer', () => {
   let state; // Default state object
 
   /**
-   * Before every test, reset state to the same defaults as 
+   * Before every test, reset state to the same defaults as
    */
   beforeEach(() => {
     state = {
       categories: [],
-    }
+    };
   });
 
   describe('default state', () => {
-    it('should return a default state when given undefined state', ()  => {
-      expect(subject(undefined, { type: undefined , payload: 'Test' })).toEqual(state);
-    })
-  })
+    it('should return a default state when given undefined state', () => {
+      expect(subject(undefined, { type: undefined, payload: 'Test' })).toEqual(
+        state
+      );
+    });
+  });
 
   describe('unrecognized action types', () => {
     it('should return the original state with no duplication', () => {
-      // Do test
+      const testAction = { type: 'fakeaction' };
+      expect(subject(state, testAction)).toBe(state);
     });
   });
 
   describe('ADD_TO_STATE', () => {
-
     // declare an add_to_state action
+    const action = {
+      type: 'ADD_TO_STATE',
+      payload: [
+        { category_id: '65', category_name: 'Applied', user_id: '2' },
+        { category_id: '66', category_name: 'In Progress', user_id: '2' },
+      ],
+    };
 
-    it('new state should be equal to passed in payload', () => {
-
+    it('new state should be equal to passed in payload with category_id and category_name', () => {
+      let result = subject(state, action).categories;
+      expect(result[0]._id).toEqual(action.payload[0].category_id);
+      expect(result[1].category_name).toEqual(action.payload[1].category_name);
     });
 
-    it('new state should not be passed in payload', () => {
-
+    it('new categories state should consist of an array of objects that have only two properties, _id, and category_name', () => {
+      let result = subject(state, action).categories;
+      console.log('result is ', result);
+      expect(Array.isArray(result)).toBe(true);
+      expect(
+        result.every(
+          (obj) =>
+            obj.hasOwnProperty('_id') &&
+            obj.hasOwnProperty('category_name') &&
+            Object.keys(obj).length === 2
+        )
+      ).toBe(true);
     });
-
-    it('new categories state should consist of an array of objects that have only two props, _id, and category_name', () => {
-
-    });
-
-    it('Array of categories should be unique', () => {
-
-    });
-  })
-
 
   describe('ADD_CATEGORY', () => {
-
     // declare an add category action that has categoryName and _id
 
-    it('Should add a category', () => {
+    xit('Should add a category', () => {
       // Do test
     });
 
-    it('Should add a category inclusive of previous categories', () => {
+    xit('Should add a category inclusive of previous categories', () => {
       // Do Test
     });
 
-    it('Final category should be equal to the action added', () => {
+    xit('Final category should be equal to the action added', () => {
       // Do Test
-    })
-
-
-  })
-
-})
+    });
+  });
+});
